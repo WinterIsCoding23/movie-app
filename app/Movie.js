88398d64f41484788400f6a747dd240d7c384cee
Markus Winter <markusdominicwinter@hotmail.com>
2023-05-15 18:54:18 +0200

    create Movie.js and move rendering inside there

---
+++ app/Movie.js
@@ -0,0 +1,18 @@
+"use client";
+import Image from "next/image";
+
+export default function Movie({ title, imagePath, poster_path }) {
+  const imagePath = "https://image.tmdb.org/t/p/original";
+
+  return (
+    <div>
+      <h2>{title}</h2>
+      <Image
+        src={imagePath + poster_path}
+        width={400}
+        height={400}
+        alt={title}
+      />
+    </div>
+  );
+}
--- app/page.js
+++ app/page.js
@@ -1,23 +1,19 @@
 import Image from "next/image";
-//import styles from "./page.module.css";
 
-export default async function Home({ title }) {
+import Movie from "./Movie";
+
+export default async function Home({ title, imagePath, poster_path }) {
   const data = await fetch(
     `https://api.themoviedb.org/3/movie/popular?api_key=${process.env.API_KEY}`
   );
 
   const response = await data.json();
 
-  const imagePath = "https://image.tmdb.org/t/p/original";
-
   return (
     <main>
       <h1>Movie-App</h1>
       {response.results.map((movie) => (
-        <div>
-          <h2>{movie.title}</h2>
-          <Image src={imagePath + movie.poster_path} width={400} height={400} />
-        </div>
+        <Movie title={title} imagePath={imagePath} poster_path={poster_path} />
       ))}
     </main>
   );
