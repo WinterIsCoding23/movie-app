df2dc69031b3aea7c107593563fce5eb6582d4c1
Markus Winter <markusdominicwinter@hotmail.com>
2023-05-16 17:00:39 +0200

    add Loading.js-file in app-folder & in RootLayout move children in separate div

--- app/layout.js
+++ app/layout.js
@@ -11,7 +11,9 @@ import "./globals.css";
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <div>{children}</div>
+      </body>
     </html>
   );
 }
