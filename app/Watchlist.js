4eece648e73bf0730c1806c729d63ad3127d6ff2
Markus Winter <markusdominicwinter@hotmail.com>
2023-05-16 19:57:43 +0200

    delete unnecessary wrapping div in Image in Watchlist.js

--- app/Watchlist.js
+++ app/Watchlist.js
@@ -10,19 +10,17 @@ export default function Watchlist({ response, data, id, title }) {
       <h2>My Watchlist</h2>
       <ul>
         {response.results.map((movie) => (
-          <div>
-            <li key={movie.id}>
-              {movie.title}
-              <Image
-                className={styles.imageContainer}
-                src={imagePath + movie.poster_path}
-                width={50}
-                height={50}
-                alt={movie.title}
-                priority={true}
-              />
-            </li>
-          </div>
+          <li key={movie.id}>
+            {movie.title}
+            <Image
+              className={styles.imageContainer}
+              src={imagePath + movie.poster_path}
+              width={50}
+              height={50}
+              alt={movie.title}
+              priority={true}
+            />
+          </li>
         ))}
       </ul>
     </div>
