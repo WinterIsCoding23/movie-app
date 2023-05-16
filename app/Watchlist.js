9d80afbb4dc79728b500caee4a8352a8f0822fd2
Markus Winter <markusdominicwinter@hotmail.com>
2023-05-16 19:42:31 +0200

    add movie-posters to Watchlist-movies (not styled)

--- app/Watchlist.js
+++ app/Watchlist.js
@@ -1,10 +1,25 @@
+import Image from "next/image";
+
 export default function Watchlist({ response, data, id, title }) {
+  const imagePath = "https://image.tmdb.org/t/p/original";
+
   return (
     <div>
       <h2>My Watchlist</h2>
       <ul>
         {response.results.map((movie) => (
-          <li key={movie.id}>{movie.title}</li>
+          <>
+            <li key={movie.id}>
+              {movie.title}
+              <Image
+                src={imagePath + movie.poster_path}
+                width={250}
+                height={250}
+                alt={movie.title}
+                priority={true}
+              />
+            </li>
+          </>
         ))}
       </ul>
     </div>
