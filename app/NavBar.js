5c7ab31bb8e3cc1f8f35008dd9b360bed92a2173
Markus Winter <markusdominicwinter@hotmail.com>
2023-05-16 13:27:49 +0200

    add icons in public & render as images in NavBar.js

--- app/NavBar.js
+++ app/NavBar.js
@@ -1,7 +1,26 @@
+import Image from "next/image";
+
 export default function NavBar() {
   return (
     <nav>
-      <h3>Here we create the navBar</h3>
+      <Image
+        src={"/../public/home-button.png"}
+        width={50}
+        height={50}
+        alt="home-button"
+      />
+      <Image
+        src={"/../public/play-button.png"}
+        width={50}
+        height={50}
+        alt="placeholder"
+      />
+      <Image
+        src={"/../public/back-button.png"}
+        width={50}
+        height={50}
+        alt="back-button"
+      />
     </nav>
   );
 }
