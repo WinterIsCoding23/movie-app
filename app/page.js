8d715594a776d94950dd89d5abbae1e28597e4ca
Markus Winter <markusdominicwinter@hotmail.com>
2023-05-17 14:12:10 +0200

    restore server-side-rendering & delete swr

--- app/page.js
+++ app/page.js
@@ -1,7 +1,4 @@
-"use client";
-
 import Image from "next/image";
-import useSWR from "swr";
 
 import styles from "./page.module.css";
 
@@ -11,26 +8,18 @@ import SearchButton from "./search-button/SearchButton";
 import Watchlist from "./watchlist/Watchlist";
 import NavBar from "./navbar/NavBar";
 
-const fetcher = (...args) => fetch(...args).then((res) => res.json());
-const URL = `https://api.themoviedb.org/3/movie/popular?api_key=${process.env.API_KEY}`;
-
 export default async function Home({ id, title }) {
-  // Worked without SWR:
-  // const data = await fetch(
-  //   `https://api.themoviedb.org/3/movie/popular?api_key=${process.env.API_KEY}`
-  // );
+  const data = await fetch(
+    `https://api.themoviedb.org/3/movie/popular?api_key=${process.env.API_KEY}`
+  );
 
-  // const response = await data.json();
-
-  const { data: results } = useSWR(URL, fetcher);
-  console.log(results);
+  const response = await data.json();
 
   return (
     <body className={styles.body}>
       <main>
         <h1 className={styles.title}>Movie-App</h1>
-        <h1>{results}</h1>
-        {/* <RandomMovie data={data} /> */}
+        <RandomMovie response={response} />
         {/* {response.results.map((movie) => (
           <Movie
             key={movie.id}
@@ -40,7 +29,7 @@ export default async function Home({ id, title }) {
           />
         ))} */}
         <SearchButton />
-        {/* <Watchlist data={data} id={id} title={title} /> */}
+        <Watchlist response={response} id={id} title={title} />
       </main>
       <footer>
         <p>Copyright 2023</p>
