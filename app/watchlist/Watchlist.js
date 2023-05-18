429c749b52a10aa5494c63e3c9098d1371c6dd3b
Markus Winter <markusdominicwinter@hotmail.com>
2023-05-18 12:40:27 +0200

    add correct routing to images in Watchlist

--- app/watchlist/Watchlist.js
+++ app/watchlist/Watchlist.js
@@ -1,6 +1,7 @@
 import Image from "next/image";
 
 import styles from "./Watchlist.module.css";
+import Link from "next/link";
 
 export default function Watchlist({ response, data, id, title }) {
   const imagePath = "https://image.tmdb.org/t/p/original";
@@ -12,13 +13,15 @@ export default function Watchlist({ response, data, id, title }) {
         {response.results.map((movie) => (
           <li key={movie.id}>
             <h3>{movie.title}</h3>
-            <Image
-              className={styles.imageContainer}
-              src={imagePath + movie.poster_path}
-              width={100}
-              height={100}
-              alt={movie.title}
-            />
+            <Link href={`/movie/${movie.id}`}>
+              <Image
+                className={styles.imageContainer}
+                src={imagePath + movie.poster_path}
+                width={100}
+                height={100}
+                alt={movie.title}
+              />
+            </Link>
           </li>
         ))}
       </ul>
