e3e05ed60ee365197ad0a29819f07e8ef2ca21ba
Markus Winter <markusdominicwinter@hotmail.com>
2023-05-21 17:47:38 +0200

    create submitButton for searchpanel & style it

--- components/Collapsible/SearchForm.js
+++ components/Collapsible/SearchForm.js
@@ -2,13 +2,31 @@ import styles from "./SearchForm.module.css";
 
 export default function SearchForm() {
   return (
-    <form className={styles.form}>
-      <label htmlFor={"title"}>by title</label>
-      <input type={"text"} id={"title"} name={"title"} />
-      <label htmlFor={"director"}>by director</label>
-      <input type={"text"} id={"director"} name={"director"} />
-      <label htmlFor={"cast"}>by cast</label>
-      <input type={"text"} id={"cast"} name={"cast"} />
-    </form>
+    <div className={styles.searchContainer}>
+      <form className={styles.form}>
+        <label htmlFor={"title"}>by title</label>
+        <input
+          type={"text"}
+          id={"title"}
+          name={"title"}
+          placeholder={"Please write here"}
+        />
+        <label htmlFor={"director"}>by director</label>
+        <input
+          type={"text"}
+          id={"director"}
+          name={"director"}
+          placeholder={"Please write here"}
+        />
+        <label htmlFor={"cast"}>by cast</label>
+        <input
+          type={"text"}
+          id={"cast"}
+          name={"cast"}
+          placeholder={"Please write here"}
+        />
+      </form>
+      <button className={styles.submitButton}>Submit search</button>
+    </div>
   );
 }
