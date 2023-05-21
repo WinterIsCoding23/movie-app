a364a8377f513e460f05bb6fbf1dd8b816cb2138
Markus Winter <markusdominicwinter@hotmail.com>
2023-05-21 16:54:53 +0200

    add flexbox to SarchForm

--- components/search-button/SearchForm.js
+++ components/search-button/SearchForm.js
@@ -1,11 +1,13 @@
+import styles from "./SearchForm.module.css";
+
 export default function SearchForm() {
   return (
-    <form>
-      <label htmlFor={"title"}>Movie title</label>
+    <form className={styles.form}>
+      <label htmlFor={"title"}>by title</label>
       <input type={"text"} id={"title"} name={"title"} />
-      <label htmlFor={"director"}>Director</label>
+      <label htmlFor={"director"}>by director</label>
       <input type={"text"} id={"director"} name={"director"} />
-      <label htmlFor={"cast"}>Cast</label>
+      <label htmlFor={"cast"}>by cast</label>
       <input type={"text"} id={"cast"} name={"cast"} />
     </form>
   );
