e5bae012d4c5fe9eb80f1f6d8af646e5ad76e216
Markus Winter <markusdominicwinter@hotmail.com>
2023-05-21 16:16:27 +0200

    add onClick-toggle to button

--- components/search-button/SearchButton.js
+++ components/search-button/SearchButton.js
@@ -6,12 +6,17 @@ import styles from "./SearchButton.module.css";
 
 export default function SearchButton() {
   const [open, setOpen] = useState(false);
+
+  const toggle = () => {
+    setOpen(!open);
+  };
+
   return (
     <div>
-      <button className={styles.button} type="button">
+      <button onClick={toggle} className={styles.button} type="button">
         <div className={styles.buttonText}>Search</div>
       </button>
-      {open && <div>toggle me</div>}
+      {open && <div className={styles.toggle}>toggle</div>}
     </div>
   );
 }
