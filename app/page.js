a6a47f8381616bf5b730cdad1bfea3df6a6ef340
Markus Winter <markusdominicwinter@hotmail.com>
2023-05-22 11:46:05 +0200

    useSWR

--- app/page.js
+++ app/page.js
@@ -1,32 +1,43 @@
+"use client";
+
+import useSWR from "swr";
+
 import styles from "./page.module.css";
 
 import RandomMovie from "../components/random-movie/RandomMovie";
 import Watchlist from "../components/Watchlist/Watchlist";
 import Collapsible from "../components/Collapsible/Collapsible";
 
-async function getData() {
-  const res = await fetch(
-    `https://api.themoviedb.org/3/movie/popular?api_key=${process.env.API_KEY}`
-  );
-  // To fetch fresh data on every fetch request, include inside fetch:
-  // { cache: "no-store" }
+// async function getData() {
+//   const res = await fetch(
+//     `https://api.themoviedb.org/3/movie/popular?api_key=${process.env.API_KEY}`
+//   );
+//   // To fetch fresh data on every fetch request, include inside fetch:
+//   // { cache: "no-store" }
 
-  if (!res.ok) {
-    throw new Error("Failed to fetch data");
-  }
-  return res.json();
-}
+//   if (!res.ok) {
+//     throw new Error("Failed to fetch data");
+//   }
+//   return res.json();
+// }
+
+// Data-fetching with SWR:
+const fetcher = (...args) => fetch(...args).then((res) => res.json());
+const URL = `https://api.themoviedb.org/3/movie/popular?api_key=${process.env.API_KEY}`;
 
 export default async function Home({ id, title }) {
-  const response = await getData();
-
+  const { data, error, isLoading } = useSWR(URL, fetcher);
+  // Data-fetching BEFORE SWR:
+  // const response = await getData();
+  console.log("data: ", data);
+  
   return (
     <>
       <main>
         <h1 className={styles.title}>Movie-App</h1>
-        <RandomMovie response={response} />
+        <RandomMovie response={data} />
         <Collapsible label={"searchForm"} />
-        <Watchlist response={response} id={id} title={title} />
+        <Watchlist response={data} id={id} title={title} />
       </main>
       <footer>
         <p>Copyright 2023</p>
