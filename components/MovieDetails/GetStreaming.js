beb0613949de02834c3593d689250c6c1c19f37c
Markus Winter <markusdominicwinter@hotmail.com>
2023-05-23 12:14:54 +0200

    delete comments

--- components/MovieDetails/GetStreaming.js
+++ components/MovieDetails/GetStreaming.js
@@ -4,9 +4,7 @@ export default async function GetStreaming({ id, imagePath, movie }) {
   const res = await fetch(
     `https://api.themoviedb.org/3/movie/${id}/watch/providers?api_key=${process.env.API_KEY}`
   );
-  // console.log("fetched getStreaming: ", res);
   const jsonData = await res.json();
-  // console.log("jsonData: ", jsonData);
   const streamingSources = jsonData?.results?.DE?.flatrate?.map(
     (element) => imagePath + element.logo_path
   );
@@ -15,7 +13,6 @@ export default async function GetStreaming({ id, imagePath, movie }) {
     "Unfortunately this movie is currently not being streamed in Germany.";
 
   if (streamingSources) {
-    // console.log("streamingSource: ", streamingSource);
     return (
       <ul style={{ listStyle: "none" }}>
         <li key={movie.id}>
