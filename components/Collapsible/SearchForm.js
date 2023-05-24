7b4db3604af9ea806bce944381b910d949cd1247
Markus Winter <markusdominicwinter@hotmail.com>
2023-05-24 23:49:57 +0200

    clean SearchForm.js from comments & save them in notes.md

--- components/Collapsible/SearchForm.js
+++ components/Collapsible/SearchForm.js
@@ -1,74 +1,9 @@
-import Link from "next/link";
-import useSWR from "swr";
-
 import styles from "./SearchForm.module.css";
 
 export default function SearchForm() {
-  // const handleSubmit = async (event) => {
-  //   event.preventDefault();
-
-  //   const data = {
-  //     title: event.target.title.value,
-  //     director: event.target.director.value,
-  //     cast: event.target.cast.value,
-  //   };
-
-  //   const JSONdata = JSON.stringify(data);
-
-  //   const endpoint = "/api/search";
-
-  //   const options = {
-  //     method: "POST",
-  //     headers: {
-  //       "Content-Type": "application/json",
-  //     },
-  //     body: JSONdata,
-  //   };
-
-  //   const response = await fetch(endpoint, options);
-
-  //   const result = await response.json();
-  //   console.log("Is this your movie-search?", result.data);
-  //   console.log("JSONdata", JSONdata);
-  // };
-
-  // const searchedMovies = useSWR("/search-results");
-
-  // async function handleSubmit(event) {
-  //   // event.preventDefault();
-
-  //   const formData = new FormData(event.target);
-  //   const movieSearchData = Object.fromEntries(formData);
-
-  //   const request = new XMLHttpRequest();
-  //   request.open("POST", "/search-results");
-  //   movieSearchData.append(); // error: Uncaught (in promise) TypeError: movieSearchData.append is not a function
-  //   request.send(movieSearchData);
-
-  //   const response = await fetch("/search-results", {
-  //     method: "GET",
-  //     // headers: {
-  //     //   "Content-Type": "application/json",
-  //     // },
-  //     // body: JSON.stringify(movieData), --> only for POST-request
-  //   });
-
-  //   if (response.ok) {
-  //     await response.json();
-  //     searchedMovies.mutate();
-  //     event.target.reset();
-  //   } else {
-  //     console.log(response.status);
-  //   }
-  // }
-
   return (
     <div className={styles.searchContainer}>
-      <form
-        className={styles.form}
-        // onSubmit={handleSubmit}
-        action="/search-results"
-      >
+      <form className={styles.form} action="/search-results">
         <label htmlFor={"title"}>by title</label>
         <input
           type={"text"}
--- notes.md
+++ notes.md
@@ -15,3 +15,63 @@ rgb(115, 95, 50)
 rgb(198, 151, 73)
 
 https://upmostly.com/next-js/how-to-level-up-your-next-js-app-with-scroll-animations
+
+From SearchForm.js:
+
+// const handleSubmit = async (event) => {
+// event.preventDefault();
+
+// const data = {
+// title: event.target.title.value,
+// director: event.target.director.value,
+// cast: event.target.cast.value,
+// };
+
+// const JSONdata = JSON.stringify(data);
+
+// const endpoint = "/api/search";
+
+// const options = {
+// method: "POST",
+// headers: {
+// "Content-Type": "application/json",
+// },
+// body: JSONdata,
+// };
+
+// const response = await fetch(endpoint, options);
+
+// const result = await response.json();
+// console.log("Is this your movie-search?", result.data);
+// console.log("JSONdata", JSONdata);
+// };
+
+// const searchedMovies = useSWR("/search-results");
+
+// async function handleSubmit(event) {
+// // event.preventDefault();
+
+// const formData = new FormData(event.target);
+// const movieSearchData = Object.fromEntries(formData);
+
+// const request = new XMLHttpRequest();
+// request.open("POST", "/search-results");
+// movieSearchData.append(); // error: Uncaught (in promise) TypeError: movieSearchData.append is not a function
+// request.send(movieSearchData);
+
+// const response = await fetch("/search-results", {
+// method: "GET",
+// // headers: {
+// // "Content-Type": "application/json",
+// // },
+// // body: JSON.stringify(movieData), --> only for POST-request
+// });
+
+// if (response.ok) {
+// await response.json();
+// searchedMovies.mutate();
+// event.target.reset();
+// } else {
+// console.log(response.status);
+// }
+// }
