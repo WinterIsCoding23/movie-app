0eac7be1cba6f59b0d6dff3b9c63580b364bdca8
Markus Winter <markusdominicwinter@hotmail.com>
2023-05-25 14:33:49 +0200

    create schema and connect.js

---
+++ db/models/watchlistMovie.js
@@ -0,0 +1,18 @@
+import Watchlist from "@/components/Watchlist/Watchlist";
+import mongoose from "mongoose";
+
+const { Schema } = mongoose;
+
+const watchlistSchema = new Schema({
+  title: { type: String, required: true },
+  image: { type: String, required: true },
+  director: { type: String, required: true },
+  genres: { type: String, required: true },
+  synopsis: { type: String, required: true },
+  streaming: { type: String, required: true },
+});
+
+const WatchlistMovies =
+  mongoose.models.Watchlist || mongoose.model("Watchlist", watchlistSchema);
+
+export default WatchlistMovies;
