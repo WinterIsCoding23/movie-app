4c23c74045fa8c371fcc4e893ffb9982af25e71f
Markus Winter <markusdominicwinter@hotmail.com>
2023-05-25 11:30:32 +0200

    style search-results-image

--- components/GetSearchResults/GetSearchResults.js
+++ components/GetSearchResults/GetSearchResults.js
@@ -4,6 +4,8 @@ import Link from "next/link";
 import Image from "next/image";
 import useSWR from "swr";
 
+import styles from "./GetSearchResults.module.css";
+
 const imagePath = "https://image.tmdb.org/t/p/original";
 
 export default async function GetSearchResults({ url, searchParams }) {
@@ -25,19 +27,26 @@ export default async function GetSearchResults({ url, searchParams }) {
   }
   return (
     <div>
-      <div>
+      <div className={styles.searchResultsContainer}>
         {data.results.map((result) => (
           <div key={result.id}>
             <h2>{result.title}</h2>
-            {console.log(imagePath + result.poster_path)}
-            <Link href={`/movie/${result.id}`}>
-              <Image
-                alt={result.title}
-                width={100}
-                height={100}
-                src={imagePath + result.poster_path}
-              />
-            </Link>
+            <div className={styles.imageContainer}>
+              <Link href={`/movie/${result.id}`}>
+                <Image
+                  className={styles.poster}
+                  // fill={true}
+                  // sizes={"(max-width: 768px) 100vw, (max-width: 1200px) 100vw"}
+                  // style={{ objectFit: "cover" }}
+                  style={{ width: "100%", height: "auto" }}
+                  sizes={"100vw"}
+                  width={0}
+                  height={0}
+                  src={imagePath + result.poster_path}
+                  alt={result.title}
+                />
+              </Link>
+            </div>
 
             <p>{result.overview}</p>
           </div>
