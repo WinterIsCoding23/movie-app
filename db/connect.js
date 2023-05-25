23b7184a097bb5cb8f0529d132f8482fe4b79be7
Markus Winter <markusdominicwinter@hotmail.com>
2023-05-25 14:50:41 +0200

    install mongoose & syntax-fix in db/connect

--- db/connect.js
+++ db/connect.js
@@ -1,14 +1,14 @@
 import mongoose from "mongoose";
 
-const MONGODB_URI = process.env.MONGODB_URI,
+const MONGODB_URI = process.env.MONGODB_URI;
 
 if (!MONGODB_URI) {
-    throw new Error(
-      "Please define the MONGODB_URI environment variable inside .env.local"
-    );
-  }
+  throw new Error(
+    "Please define the MONGODB_URI environment variable inside .env.local"
+  );
+}
 
-  let cached = global.mongoose;
+let cached = global.mongoose;
 
 if (!cached) {
   cached = global.mongoose = { conn: null, promise: null };
