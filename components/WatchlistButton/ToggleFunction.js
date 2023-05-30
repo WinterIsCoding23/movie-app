63972fef85fa2b226b3017c26e08ea1ea8db9755
Markus Winter <markusdominicwinter@hotmail.com>
2023-05-30 11:28:28 +0200

    move WatchlistButton and NoWatchlistButton into ToggleFunction & toggle works

--- components/WatchlistButton/ToggleFunction.js
+++ components/WatchlistButton/ToggleFunction.js
@@ -1,11 +1,11 @@
 "use client";
 
-import WatchlistButton from "./WatchlistButton";
-import NoWatchlistButton from "./NoWatchlistButton";
+import Image from "next/image";
 import { useState } from "react";
 
 export default async function ToggleButton({ id, isFavorite }) {
   console.log("id in ToggleFunction.js:", id);
+  console.log("isFavorite:", isFavorite);
 
   //GET NextResponse-object from mongoDB
   // const responseData = await fetch(`/api/watchlist/${id}`, { method: "GET" });
@@ -13,27 +13,55 @@ export default async function ToggleButton({ id, isFavorite }) {
   // const checkFavorite = response?.isFavorite ? response.isFavorite : false;
   // console.log("checkFavorite:", checkFavorite);
 
-  // const [isFavorite, setFavorite] = useState(checkFavorite);
+  const [watchlistFavorite, setWatchlistFavorite] = useState(isFavorite);
 
   const toggleFavorite = () => {
-    setFavorite((isFavorite) => {
-      if (isFavorite === true) {
+    setWatchlistFavorite((watchlistFavorite) => {
+      if (watchlistFavorite === true) {
         console.log("Remove movie from Watchlist");
-      } else if (isFavorite === false) {
+      } else if (watchlistFavorite === false) {
         console.log("Add movie to Watchlist");
       }
       fetch(`/api/watchlist/${id}`, {
         method: "PUT",
-        body: JSON.stringify({ isFavorite: !isFavorite }),
+        body: JSON.stringify({ watchlistFavorite: !watchlistFavorite }),
       });
-      return !isFavorite;
+      return !watchlistFavorite;
     });
   };
 
-  return isFavorite === false ? (
-    <NoWatchlistButton onClick={() => toggleFavorite()} key={id} />
+  function WatchlistButton() {
+    return (
+      <button type="button" onClick={() => toggleFavorite()}>
+        <Image
+          src={"/../public/director-chair-empty.png"}
+          width={30}
+          height={30}
+          alt={"already-a-favorite"}
+        />
+        <p>"un-favor movie"</p>
+      </button>
+    );
+  }
+
+  function NoWatchlistButton() {
+    return (
+      <button type="button" onClick={() => toggleFavorite()}>
+        <Image
+          src={"/../public/director-chair-filled.png"}
+          width={30}
+          height={30}
+          alt={"no-favorite-yet"}
+        />
+        <p>"make favorite"</p>
+      </button>
+    );
+  }
+
+  return watchlistFavorite === false ? (
+    <NoWatchlistButton />
   ) : (
-    <WatchlistButton onClick={() => toggleFavorite()} key={id} />
+    <WatchlistButton />
   );
   // <button type="button" onClick={() => toggleFavorite()} key={"Movie-Id"}>
   //   {favorite === true ? noWatchlistMovie : watchlistMovie}
