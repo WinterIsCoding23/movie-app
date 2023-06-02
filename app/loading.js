171acf9d4f939ac11045230731d71585a29b5d70
Markus Winter <markusdominicwinter@hotmail.com>
2023-06-02 12:35:08 +0200

    modify loading.js

--- app/loading.js
+++ app/loading.js
@@ -1,3 +1,7 @@
 export default function Loading() {
-  return <p>Loading... we recommend you prepare your drinks and snacks in the meantime.</p>;
+  return (
+    <p>
+      Loading... go ahead and prepare your drinks and snacks in the meantime.
+    </p>
+  );
 }
