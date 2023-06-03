f3513a414ca6f689870b4624c053c4dc9a1e5cc9
Markus Winter <markusdominicwinter@hotmail.com>
2023-06-03 15:08:31 +0200

    use custom prevArrow and nextArrow

--- components/Watchlist/Watchlist.js
+++ components/Watchlist/Watchlist.js
@@ -27,12 +27,77 @@ export default function Watchlist() {
 
   const imagePath = "https://image.tmdb.org/t/p/original";
 
+  const SlickArrowLeft = ({ currentSlide, slideCount, ...props }) => (
+    <button
+      {...props}
+      className={
+        "slick-prev slick-arrow" + (currentSlide === 0 ? " slick-disabled" : "")
+      }
+      aria-hidden="true"
+      aria-disabled={currentSlide === 0 ? true : false}
+      type="button"
+    >
+      Previous
+    </button>
+  );
+  const SlickArrowRight = ({ currentSlide, slideCount, ...props }) => (
+    <button
+      {...props}
+      className={
+        "slick-next slick-arrow" +
+        (currentSlide === slideCount - 1 ? " slick-disabled" : "")
+      }
+      aria-hidden="true"
+      aria-disabled={currentSlide === slideCount - 1 ? true : false}
+      type="button"
+    >
+      Next
+    </button>
+  );
+
   const settings = {
     dots: true,
-    infinite: true,
+
     speed: 500,
-    slidesToShow: 1,
+
     slidesToScroll: 1,
+    initialSlide: 0,
+    className: "center",
+    centerMode: true,
+    infinite: true,
+    centerPadding: "60px",
+    slidesToShow: 3,
+    speed: 500,
+
+    prevArrow: <SlickArrowLeft />,
+    nextArrow: <SlickArrowRight />,
+
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          initialSlide: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
   };
 
   return (
