cad6f19376403c2ebc379426679528c5299fa5b5
Markus Winter <markusdominicwinter@hotmail.com>
2023-06-03 15:58:55 +0200

    position prev-slick-arrow and next-slick-arrow on top and underneath of slider

--- components/Watchlist/Watchlist.js
+++ components/Watchlist/Watchlist.js
@@ -31,7 +31,7 @@ export default function Watchlist() {
     <button
       {...props}
       className={
-        "slick-prev slick-arrow" + (currentSlide === 0 ? " slick-disabled" : "")
+        "prev-slick-arrow" + (currentSlide === 0 ? " slick-disabled" : "")
       }
       aria-hidden="true"
       aria-disabled={currentSlide === 0 ? true : false}
@@ -44,7 +44,7 @@ export default function Watchlist() {
     <button
       {...props}
       className={
-        "slick-next slick-arrow" +
+        "next-slick-arrow" +
         (currentSlide === slideCount - 1 ? " slick-disabled" : "")
       }
       aria-hidden="true"
