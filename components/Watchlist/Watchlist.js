f78b9b90c70cdaf9f5bf3d2826b02bc1adc7e450
Markus Winter <markusdominicwinter@hotmail.com>
2023-06-03 14:55:01 +0200

    add react-slick for the slider

--- components/Watchlist/Watchlist.js
+++ components/Watchlist/Watchlist.js
@@ -28,66 +28,19 @@ export default function Watchlist() {
   const imagePath = "https://image.tmdb.org/t/p/original";
 
   const settings = {
-    dots: false,
+    dots: true,
     infinite: true,
-    slidesToShow: calculateSlidesToShow(),
+    speed: 500,
+    slidesToShow: 1,
     slidesToScroll: 1,
-    centerMode: true,
-    centerPadding: "60px",
-    responsive: [
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: calculateSlidesToShow(1024),
-        },
-      },
-      {
-        breakpoint: 768,
-        settings: {
-          slidesToShow: calculateSlidesToShow(768),
-        },
-      },
-    ],
-    customPaging: function (i) {
-      return (
-        <div className={styles.customDot}>
-          <Image
-            src={moviesOnWatchlist[i].image}
-            width={50}
-            height={50}
-            alt={moviesOnWatchlist[i].title}
-          />
-        </div>
-      );
-    },
-    appendDots: (dots) => <ul className={styles.dotList}>{dots}</ul>,
-    beforeChange: function (currentSlide, nextSlide) {
-      const slideList = document.querySelectorAll(".slick-slide");
-
-      slideList.forEach((slide, index) => {
-        if (index === currentSlide || index === nextSlide) {
-          slide.style.filter = "none";
-        } else {
-          slide.style.filter = "blur(2px)";
-        }
-      });
-    },
   };
-
-  function calculateSlidesToShow(breakpointWidth = Infinity) {
-    const viewportWidth = Math.min(window.innerWidth, breakpointWidth);
-    const slideWidth = 300; // Adjust this value to your desired slide width
-    const margin = 20; // Adjust this value to your desired slide margin
-
-    return Math.floor(viewportWidth / (slideWidth + margin));
-  }
 
   return (
     <div className={styles.watchlistContainer}>
       <h2 className={styles.watchlistHeader}>My Watchlist</h2>
       <Slider {...settings}>
         {moviesOnWatchlist.map((movieOnWatchlist, index) => (
-          <div key={index} className={styles.slide}>
+          <div key={index}>
             <h3>{movieOnWatchlist.title}</h3>
             <Link href={`/movie/${movieOnWatchlist.id}`}>
               <Image
@@ -102,14 +55,6 @@ export default function Watchlist() {
           </div>
         ))}
       </Slider>
-      <style jsx>{`
-        .slick-next {
-          right: 10px; /* Adjust the position as needed */
-          top: 50%;
-          transform: translateY(-50%);
-          z-index: 1;
-        }
-      `}</style>
     </div>
   );
 }
