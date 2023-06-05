c1a15821930788e2ae52131a60382df1c7890726
Markus Winter <markusdominicwinter@hotmail.com>
2023-06-05 12:06:01 +0200

    fix slider - but still not showing missing poster correctly

--- components/Watchlist/Watchlist.js
+++ components/Watchlist/Watchlist.js
@@ -3,7 +3,10 @@
 import useSWR from "swr";
 import Link from "next/link";
 import Image from "next/image";
+
+import { useState } from "react";
 import Slider from "react-slick";
+// import { FaChevronLeft, FaChevronRight } from "react-icons";
 // import React, { Component } from "react";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
@@ -11,6 +14,9 @@ import "slick-carousel/slick/slick-theme.css";
 import styles from "./Watchlist.module.css";
 
 export default function Watchlist() {
+  // useState for Slider
+  const [sliderRef, setSliderRef] = useState(null);
+
   const fetcher = (...args) => fetch(...args).then((res) => res.json());
   const {
     data: moviesOnWatchlist,
@@ -29,48 +35,60 @@ export default function Watchlist() {
   const imagePath = "https://image.tmdb.org/t/p/original";
 
   // Slick-Slider:
-  const SlickArrowLeft = ({ currentSlide, slideCount, ...props }) => (
-    <button
-      {...props}
-      className={
-        "prev-slick-arrow" + (currentSlide === 0 ? " slick-disabled" : "")
-      }
-      aria-hidden="true"
-      aria-disabled={currentSlide === 0 ? true : false}
-      type="button"
-    >
-      Previous
-    </button>
-  );
-  const SlickArrowRight = ({ currentSlide, slideCount, ...props }) => (
-    <button
-      {...props}
-      className={
-        "next-slick-arrow" +
-        (currentSlide === slideCount - 1 ? " slick-disabled" : "")
-      }
-      aria-hidden="true"
-      aria-disabled={currentSlide === slideCount - 1 ? true : false}
-      type="button"
-    >
-      Next
-    </button>
-  );
-
-  const settings = {
+  const sliderSettings = {
     dots: true,
-    speed: 500,
+    arrows: false,
+    slidesToShow: 3,
     slidesToScroll: 1,
     initialSlide: 0,
-    className: "center",
-    centerMode: true,
+    speed: 500,
     infinite: true,
     centerPadding: "60px",
-    slidesToShow: 3,
-    speed: 500,
+    // className: "center",
+    // centerMode: true,
 
-    prevArrow: <SlickArrowLeft />,
-    nextArrow: <SlickArrowRight />,
+    // const SlickArrowLeft = ({ currentSlide, slideCount, ...props }) => (
+    //   <button
+    //     {...props}
+    //     className={
+    //       "prev-slick-arrow" + (currentSlide === 0 ? " slick-disabled" : "")
+    //     }
+    //     aria-hidden="true"
+    //     aria-disabled={currentSlide === 0 ? true : false}
+    //     type="button"
+    //   >
+    //     Previous
+    //   </button>
+    // );
+    // const SlickArrowRight = ({ currentSlide, slideCount, ...props }) => (
+    //   <button
+    //     {...props}
+    //     className={
+    //       "next-slick-arrow" +
+    //       (currentSlide === slideCount - 1 ? " slick-disabled" : "")
+    //     }
+    //     aria-hidden="true"
+    //     aria-disabled={currentSlide === slideCount - 1 ? true : false}
+    //     type="button"
+    //   >
+    //     Next
+    //   </button>
+    // );
+
+    // const settings = {
+    //   dots: true,
+    //   speed: 500,
+    //   slidesToScroll: 1,
+    //   initialSlide: 0,
+    //   className: "center",
+    //   centerMode: true,
+    //   infinite: true,
+    //   centerPadding: "60px",
+    //   slidesToShow: 3,
+    //   speed: 500,
+
+    // prevArrow: <SlickArrowLeft />,
+    // nextArrow: <SlickArrowRight />,
 
     responsive: [
       {
@@ -78,6 +96,7 @@ export default function Watchlist() {
         settings: {
           slidesToShow: 3,
           slidesToScroll: 1,
+          initialSlide: 0,
           infinite: true,
           dots: true,
         },
@@ -85,16 +104,17 @@ export default function Watchlist() {
       {
         breakpoint: 600,
         settings: {
-          slidesToShow: 2,
+          slidesToShow: 3,
           slidesToScroll: 1,
-          initialSlide: 2,
+          initialSlide: 0,
         },
       },
       {
         breakpoint: 480,
         settings: {
-          slidesToShow: 1,
+          slidesToShow: 2,
           slidesToScroll: 1,
+          initialSlide: 0,
         },
       },
     ],
@@ -107,24 +127,32 @@ export default function Watchlist() {
     <div className={styles.watchlistContainer}>
       <h2 className={styles.watchlistHeader}>My Watchlist</h2>
       {moviesOnWatchlist.length > 0 ? (
-        <Slider className={styles.slider} {...settings}>
-          {moviesOnWatchlist.map((movieOnWatchlist, index) => (
-            <div key={index}>
-              <h3>{movieOnWatchlist.title}</h3>
-              <Link href={`/movie/${movieOnWatchlist.id}`}>
-                <Image
-                  className={styles.watchlistPoster}
-                  src={movieOnWatchlist.image}
-                  width={250}
-                  height={250}
-                  alt={movieOnWatchlist.title}
-                  priority={true}
-                />
-              </Link>
-              {/* <p>{String(movieOnWatchlist.isFavorite)}</p> */}
-            </div>
-          ))}
-        </Slider>
+        <div>
+          <button onClick={sliderRef?.slickPrev}>Left</button>
+          <button onClick={sliderRef?.slickNext}>Right</button>
+          <Slider
+            className={styles.slider}
+            ref={setSliderRef}
+            {...sliderSettings}
+          >
+            {moviesOnWatchlist.map((movieOnWatchlist, index) => (
+              <div key={index}>
+                <h3>{movieOnWatchlist.title}</h3>
+                <Link href={`/movie/${movieOnWatchlist.id}`}>
+                  <Image
+                    className={styles.watchlistPoster}
+                    src={movieOnWatchlist.image}
+                    width={250}
+                    height={250}
+                    alt={movieOnWatchlist.title}
+                    priority={true}
+                  />
+                </Link>
+                {/* <p>{String(movieOnWatchlist.isFavorite)}</p> */}
+              </div>
+            ))}
+          </Slider>
+        </div>
       ) : (
         <p className={styles.noMoviesOnWatchlist}>
           Nothing to show here 😪 - how about you start filling your Watchlist?
