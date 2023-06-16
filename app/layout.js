8153ed7014e1af00d9dd034a89f4e1bd8ec523b1
Markus Winter <markusdominicwinter@hotmail.com>
2023-06-16 18:24:22 +0200

    delete comments

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,6 @@
 import "./global.css";
 import NavBar from "../components/Navbar/NavBar";
-//
+
 // import { Inter } from "next/font/google";
 
 // const inter = Inter({ subsets: ['latin'] })
@@ -10,8 +10,6 @@ import NavBar from "../components/Navbar/NavBar";
 //     icon: "",
 //   },
 // };
-//nextjs.org/docs/app/api-reference/functions/generate-metadata#icon
-// https://nextjs.org/docs/app/api-reference/file-conventions/metadata/app-icons#icon
 
 export default function RootLayout({ children }) {
   return (
