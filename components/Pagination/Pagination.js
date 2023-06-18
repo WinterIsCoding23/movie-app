b898768d93569d4e16c238e4419e991c101fea5c
Markus Winter <markusdominicwinter@hotmail.com>
2023-06-18 21:14:37 +0200

    do prettiers job for the Image-components, part 2

--- components/Pagination/Pagination.js
+++ components/Pagination/Pagination.js
@@ -14,27 +14,24 @@ export default function Pagination({ page, totalPages, onPageChange }) {
       }
     }
   
-    return (
-        
-
-      <div className={styles.container}>
-        <button className={styles.backButton} onClick={backwards}>{            
-            <Image
-              src={"/../public/previous.png"}
-              width={20}
-              height={20}
-              alt={"previous"}
-            />}</button>
-        <p>{page}/{totalPages}</p>
-        <button className={styles.forwardButton} onClick={forward}>{            
-            <Image
-              src={"/../public/next.png"}
-              width={20}
-              height={20}
-              alt={"next"}
-            />}</button>
-      </div>
-        
+    return (      
+        <div className={styles.container}>
+            <button className={styles.backButton} onClick={backwards}>{            
+                <Image
+                src={"/../public/previous.png"}
+                width={20}
+                height={20}
+                alt={"previous"}
+                />}</button>
+            <p>{page}/{totalPages}</p>
+            <button className={styles.forwardButton} onClick={forward}>{            
+                <Image
+                src={"/../public/next.png"}
+                width={20}
+                height={20}
+                alt={"next"}
+                />}</button>
+        </div>        
     );
   }
   
